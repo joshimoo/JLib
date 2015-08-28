4ff36cc78954762f23beec71955e6e82769028c3
Joshua Moody <joshimoo@hotmail.de>
2015-08-28 19:54:02 +0200

    Implemented BinarySearch for Generic Arrays

---
+++ src/com/sleepycoders/jlib/algorithm/search/BinarySearch.java
@@ -0,0 +1,44 @@
+package com.sleepycoders.jlib.algorithm.search;
+import java.util.Comparator;
+
+/**
+ * @author Joshua Moody (joshimoo@hotmail.de)
+ */
+public final class BinarySearch {
+
+    public static final int NOT_FOUND = -1;
+
+    private BinarySearch() {}
+
+    public static <T extends Comparable<? super T>> int Search(T[] data, T elem) {
+        int low = 0;
+        int high = data.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int c = elem.compareTo(data[mid]);
+
+            if (c < 0) { high = mid - 1; }
+            else if (c > 0) { low = mid + 1; }
+            else { return mid; }
+        }
+
+        return NOT_FOUND;
+    }
+
+    public static <T> int Search(T[] data, T elem, Comparator<? super T> cmp) {
+        int low = 0;
+        int high = data.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int c = cmp.compare(elem, data[mid]);
+
+            if (c < 0) { high = mid - 1; }
+            else if (c > 0) { low = mid + 1; }
+            else { return mid; }
+        }
+
+        return NOT_FOUND;
+    }
+}
