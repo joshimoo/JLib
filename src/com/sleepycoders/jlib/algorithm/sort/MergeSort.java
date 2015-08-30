bc00d813ca28c2e1c22d13f49f4ee2fa506162e8
Joshua Moody <joshimoo@hotmail.de>
2015-08-30 22:04:31 +0200

    Implemented MergeSort for Generic Arrays
    Went for a recursive implementation with an iterative Merge Routine, that uses O(n) extra memory

---
+++ src/com/sleepycoders/jlib/algorithm/sort/MergeSort.java
@@ -0,0 +1,66 @@
+package com.sleepycoders.jlib.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * Recursive MergeSort Implementation with an iterative merge routine
+ * That requires O(n) extra Memory
+ * @author Joshua Moody (joshimoo@hotmail.de)
+ */
+public final class MergeSort {
+
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private MergeSort() {}
+
+
+    /**
+     * Merge Sorts the complete data Array
+     * uses a Merge Routine with O(n) extra memory
+     * Runtime Worst-Case: O(n*log(n))
+     */
+    public static <T extends Comparable<? super T>> void Sort(T[] data) {
+        Sort(data, 0, data.length - 1);
+    }
+
+    /**
+     * Merge Sort the area between low (inclusive) and high (inclusive)
+     * @param data source and destination
+     * @param low lower index inclusive
+     * @param high upper index inclusive
+     */
+    public static <T extends Comparable<? super T>> void Sort(T[] data, int low, int high) {
+        // Base Case - 1 Element List is always sorted
+        if (low >= high) { return; }
+        int mid = low + (high - low) / 2;
+        Sort(data, low, mid);
+        Sort(data, mid + 1, high);
+        Merge(data, low, mid, high);
+    }
+
+    /**
+     * Merge Routine, with O(n) extra memory
+     * Left Segment = low to mid;
+     * Right Segment = mid + 1 to high;
+     * @param data source and destination
+     * @param low lower index inclusive
+     * @param mid mid index part of lower segment inclusive
+     * @param high upper index inclusive
+     */
+    private static <T extends Comparable<? super T>> void Merge(T[] data, int low, int mid, int high) {
+        T[] left = Arrays.copyOfRange(data, low, mid + 1);
+        T[] right = Arrays.copyOfRange(data, mid + 1, high + 1);
+
+        for (int i = low, l = 0, r = 0; i <= high; i++) {
+            if (l < left.length && r < right.length) {
+                if (left[l].compareTo(right[r]) > 0) { data[i] = right[r++]; }
+                else { data[i] = left[l++]; }
+            } else if (l < left.length) {
+                data[i] = left[l++];
+            } else if(r < right.length) {
+                data[i] = right[r++];
+            }
+        }
+    }
+}
