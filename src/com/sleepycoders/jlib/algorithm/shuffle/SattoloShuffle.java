a50fc227c9c64764834cec8e8f10add73e43e95b
Joshua Moody <joshimoo@hotmail.de>
2015-09-10 20:34:34 +0200

    Implemented SattoloShuffle for Generic Arrays
    SattoloShuffle guarantees that each item, is moved at least once.
    Therefore the output can never be equal to input
    Also added a ConstantRandom Number Generator
    Which will always return 0 together with a sattolo shuffle
    This will lead to a left rotation of the array by 1

--- src/com/sleepycoders/jlib/algorithm/shuffle/SattoloShuffle.java
+++ src/com/sleepycoders/jlib/algorithm/shuffle/SattoloShuffle.java
@@ -5,18 +5,22 @@ import com.sleepycoders.jlib.util.Arrays;
 import java.util.Random;
 
 /**
- * FisherYates Shuffle (Knuth) O(n) implementation
+ * The difference beetwen a Knuth and Sattolo Shuffle,
+ * Is that the Sattolo Shuffle, is current index exclusive
+ * And therefore results in a permutation that always consists of a single cycle
  * @author Joshua Moody (joshimoo@hotmail.de)
  */
-public final class KnuthShuffle {
+public final class SattoloShuffle {
 
     /**
      * Don't let anyone instantiate this class.
      */
-    private KnuthShuffle() {}
+    private SattoloShuffle() {}
+
 
     /**
      * Uses the default System PRNG
+     * Guarantees that each item is moved at least once
      */
     public static <T> void shuffle(T[] data) {
         shuffle(data, new Random());
@@ -25,15 +29,15 @@ public final class KnuthShuffle {
     /**
      * The Randomness is higly tied to the used PRNG
      * And the used internal state bits, where applicable.
-     * For example to perfectly shuffle a deck of 52 cards, Would require 52! ≈ 2225.6 possible permutations.
+     * For example to perfectly shuffle a deck of 52 cards, Would require 52! ? 2225.6 possible permutations.
      * It's impossible for a generator with less than 226 bits of internal state to produce all the possible permutations of a 52-card deck.
      * @param rng a pre initialized PRNG
      */
     public static <T> void shuffle(T[] data, Random rng) {
         assert data != null && rng != null;
         for (int i = data.length - 1; i > 0; i--) {
-            // upperbound is i (inclusive)
-            Arrays.swap(data, rng.nextInt(i + 1), i);
+            // upperbound is (i-1) (inclusive), therefore every item will be moved at least once
+            Arrays.swap(data, rng.nextInt(i), i);
         }
     }
 }
---
+++ test/com/sleepycoders/jlib/algorithm/shuffle/SattoloShuffleTest.java
@@ -0,0 +1,57 @@
+package com.sleepycoders.jlib.algorithm.shuffle;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Joshua Moody (joshimoo@hotmail.de)
+ */
+public class SattoloShuffleTest {
+
+    @Test
+    public void testShuffle() throws Exception {
+        // In a Satollo Shuffle the Array cannot be the same, since each item moves at least once
+        Integer[] actual = new Integer[] {1, 2, 3, 4};
+        Integer[] expected = new Integer[] {1, 2, 3, 4};
+        SattoloShuffle.shuffle(actual);
+        assertThat(Arrays.asList(actual), hasItems(expected));
+        assertThat(Arrays.asList(actual), not(expected));
+    }
+
+    @Test
+    public void testShuffleEmpty() throws Exception {
+        Integer[] actual = new Integer[0];
+        Integer[] expected = new Integer[0];
+        SattoloShuffle.shuffle(actual);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testShuffleCustomRng() throws Exception {
+        Integer[] actual = new Integer[] {1,2,3,4};
+        Integer[] expected = new Integer[] {2,3,4,1};
+        SattoloShuffle.shuffle(actual, new LeftRotateRandom());
+        assertThat(actual, is(expected));
+    }
+
+    /**
+     * Will always return 0 together with a sattolo shuffle
+     * This will lead to a left rotation of the array by 1
+     */
+    private class LeftRotateRandom extends Random {
+        private int[] pool = new int[] {0};
+        private int index = 0;
+
+        @Override
+        public int nextInt(int bound) {
+            return pool[index++ % pool.length] % bound;
+        }
+    }
+}
